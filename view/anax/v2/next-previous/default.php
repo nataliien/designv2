de762a9d2389f85e53937786576cd82c1678ad65
Natalie Nikou <natalienikou@Natalies-MacBook-Pro-2.local>
2018-11-20 19:02:01 +0800

    Commit changes to kmom03

--- view/anax/v2/next-previous/default.php
+++ view/anax/v2/next-previous/default.php
@@ -16,22 +16,21 @@ if (isset($class)) {
 }
 
 // Prepare titles
-$nextTitle = $next["title"];
 $prevTitle = $previous["title"];
+$nextTitle = $next["title"];
 
 if (isset($nextPreviousTitle) && $nextPreviousTitle === false) {
-    $nextTitle = t("Next");
     $prevTitle = t("Previous");
+    $nextTitle = t("Next");
 }
 
 
 
 ?><div <?= classList($classes) ?>>
-    <?php if (isset($next)) : ?>
-        <div class="next"><a href="<?= url($next["route"]) ?>"><?= $nextTitle ?></a> »</div>
-    <?php endif; ?>
-
     <?php if (isset($previous)) : ?>
         <div class="previous">« <a href="<?= url($previous["route"]) ?>"><?= $prevTitle ?></a></div>
+    <?php endif; ?>
+    <?php if (isset($next)) : ?>
+        <div class="next"><a href="<?= url($next["route"]) ?>"><?= $nextTitle ?></a> »</div>
     <?php endif; ?>
 </div>
